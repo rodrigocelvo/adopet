7088422289cec18e9b959c365f8eb1fd2cc39ba8
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-10-16 18:28:33 -0300

    chore: create mobile app

---
+++ mobile/src/theme/index.ts
@@ -0,0 +1,37 @@
+export const THEME = {
+  COLORS: {
+    BACKGROUND: "#f6f6f6",
+
+    TEXT: "#2C3232",
+
+    CAPTION_500: "#71717A",
+    CAPTION_400: "#A1A1AA",
+    CAPTION_300: "#D4D4D8",
+
+    SHAPE: "#2A2634",
+    HEADING: "#FFFFFF",
+
+    PRIMARY_50: "#E6FEFF",
+    PRIMARY_100: "#CCF8F9",
+    PRIMARY_500: "#5AA9EF",
+    PRIMARY_900: "#00B6BA",
+
+    SUCCESS: "#34D399",
+    ALERT: "#F87171",
+
+    FOOTER: ["rgba(0,0,0,0)", "rgba(0,0,0,0.9)"],
+    OVERLAY: "rgba(0,0,0,0.6)",
+  },
+
+  FONT_FAMILY: {
+    REGULAR: "Poppins_400Regular",
+    BOLD: "Poppins_700Bold",
+    BLACK: "Poppins_900Black",
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 24,
+  },
+};
