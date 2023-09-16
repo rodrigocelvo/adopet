1fdd07437831ec35bfcf371ed71668742b26c44c
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2023-09-16 15:20:45 -0300

    remove: java backend

--- backend/src/main/java/com/fecaf/adopet/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fecaf.adopet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.fecaf.adopet.model.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {}
