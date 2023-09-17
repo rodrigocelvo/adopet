d7175c3bb2ffa63e6ac482726ebe9ff7980b233d
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2023-09-16 22:03:52 -0300

    chore: upload router

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,51 +1,87 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import multer from 'fastify-multer';
-import fs from 'fs';
-import path from 'path';
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import multer from "fastify-multer";
+import fs from "fs";
+import path from "path";
+import { z } from "zod";
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 export async function uploadRoutes(fastify: FastifyInstance) {
   fastify.get(
-    '/images/:fileName',
+    "/images/:fileName",
     async (
       request: FastifyRequest<{ Params: { fileName: string } }>,
-      reply: FastifyReply,
+      reply: FastifyReply
     ) => {
       const fileName = request.params.fileName;
 
-      const filePath = path.join(__dirname, 'uploads', fileName);
+      const filePath = path.join(__dirname, "uploads", "users", fileName);
 
       reply.sendFile(filePath);
-    },
+    }
   );
 
   fastify.post(
-    '/upload',
-    { preHandler: upload.single('image') },
+    "/uploads/pet",
+    { preHandler: upload.single("photo") },
     async (request: any, reply) => {
       const file = request.file;
 
       if (!file) {
-        reply.code(400).send({ error: 'No image available' });
+        reply.code(400).send({ error: "No image available" });
         return;
       }
 
       const fileName = file.originalname;
-      const fileExtension = fileName.substring(fileName.lastIndexOf('.'));
+      const fileExtension = fileName.substring(fileName.lastIndexOf("."));
 
-      const newFileName = `avatar-${Date.now()}${fileExtension}`;
+      const newFileName = `pet-${Date.now()}${fileExtension}`;
       const oldPath = file.path;
-      const newPath = `uploads/${newFileName}`;
+      const newPath = `uploads/pets/${newFileName}`;
 
-      fs.rename(oldPath, newPath, err => {
+      fs.rename(oldPath, newPath, (err) => {
         if (err) {
           console.error(err);
-          reply.code(500).send({ error: 'Error saving the image' });
+          reply.code(500).send({ error: "Error saving the image" });
           return;
         }
       });
       reply.code(201).send({ image: newFileName });
-    },
+    }
+  );
+
+  fastify.post(
+    "/uploads/users/:id",
+    { preHandler: upload.single("image") },
+    async (request: any, reply) => {
+      const file = request.file;
+
+      if (!file) {
+        reply.code(400).send({ error: "No image available" });
+        return;
+      }
+
+      const idUserParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = idUserParams.parse(request.params);
+
+      const fileName = file.originalname;
+      const fileExtension = fileName.substring(fileName.lastIndexOf("."));
+
+      const newFileName = `${id}${fileExtension}`;
+      const oldPath = file.path;
+      const newPath = `uploads/users/${newFileName}`;
+
+      fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+          console.error(err);
+          reply.code(500).send({ error: "Error saving the image" });
+          return;
+        }
+      });
+      reply.code(201).send({ image: newFileName });
+    }
   );
 }
